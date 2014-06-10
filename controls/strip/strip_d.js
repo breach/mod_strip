bab1fa7d2c3131df27697e2065ac5187b44f1558
Stanislas Polu <stan@teleportd.com>
2014-06-10 11:57:12 -0700

    Tabs scrolling

--- controls/strip/strip_d.js
+++ controls/strip/strip_d.js
@@ -33,6 +33,10 @@ angular.module('breach.directives').controller('StripCtrl',
     /* Dictionary of tabs div elements. */
     var tabs_divs = {};
 
+    /* Handy divs */
+    var wrapper_div = $($element).find('.wrapper');
+    var tabs_div = $($element).find('.tabs');
+
     // ### create_tab
     //
     // Creates a new tab div element with the specified id
@@ -148,6 +152,24 @@ angular.module('breach.directives').controller('StripCtrl',
     var position_tab = function(tab_id, idx) {
       var tab = tabs_divs[tab_id];
       tab.css('left', idx * (TAB_WIDTH + TAB_MARGIN));
+      /* If this is the active tab, we make sure it is visible. */
+      if($scope.active === tab_id) {
+        var tabs_width = Object.keys(tabs_divs).length * (TAB_WIDTH + TAB_MARGIN);
+        var tabs_left = tabs_div.position().left;
+
+        if((idx + 1) * (TAB_WIDTH + TAB_MARGIN) + tabs_left > wrapper_div.width()) {
+          tabs_div.css({ 
+            'transition': 'left 0.2s',
+            'left': (wrapper_div.width() - (idx + 1) * (TAB_WIDTH + TAB_MARGIN)) + 'px'
+          });
+        }
+        else if(-tabs_left > idx * (TAB_WIDTH + TAB_MARGIN)) {
+          tabs_div.css({ 
+            'transition': 'left 0.2s',
+            'left': (idx * (TAB_WIDTH + TAB_MARGIN)) + 'px'
+          });
+        }
+      }
     };
 
     // ### position_new_tab
@@ -169,6 +191,30 @@ angular.module('breach.directives').controller('StripCtrl',
       tab.remove();
     };
 
+    // ### mousewheel_handler
+    //
+    // Handles the mousewheel events to scroll tabs
+    // ```
+    // @evt {object} the jquery event
+    // ```
+    var mousewheel_handler = function(evt) {
+      var tabs_width = Object.keys(tabs_divs).length * (TAB_WIDTH + TAB_MARGIN);
+      var tabs_left = tabs_div.position().left;
+
+      var update = tabs_left + evt.originalEvent.wheelDeltaX;
+      if(wrapper_div.width() - update > tabs_width) {
+        update = wrapper_div.width() - tabs_width;
+      }
+      if(update > 0) {
+        update = 0;
+      }
+      tabs_div.css({ 
+        'transition': 'none',
+        'left': (update) + 'px'
+      });
+    };
+
+    wrapper_div.bind('mousewheel', mousewheel_handler);
 
     /**************************************************************************/
     /* ANGULAR INTEGRATION */
@@ -180,9 +226,6 @@ angular.module('breach.directives').controller('StripCtrl',
         /* Create any missing tab. */
         state.tabs.forEach(function(t) {
           tabs_data[t.tab_id] = t;
-          if(t && t.state && t.state.loading) {
-            console.log(t);
-          }
           tabs_order.push(t.tab_id);
           if(!tabs_divs[t.tab_id]) {
             tabs_divs[t.tab_id] = create_tab(t.tab_id);
