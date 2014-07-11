005d177ee49c9ab18b40d0382130d20f50ada540
Avinash Kundaliya <avinash@avinash.com.np>
2014-07-12 00:40:18 +0200

    fix for issue #21, cant click on filtered tab

--- controls/strip/strip_c.js
+++ controls/strip/strip_c.js
@@ -81,7 +81,7 @@ var strip_c = function(spec, my) {
     var tab = $('<div/>')
       .attr('id', tab_id)
       .addClass('tab')
-      .click(function(event) {
+      .mousedown(function(event) {
         switch(event.which) {
             case 1:
               select_tab(tab_id);
